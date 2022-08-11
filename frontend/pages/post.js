777e9c2a1a9c2b35b735fcbe279f838fa523bf09
Hassan-jr <abdiladifhassan115@gmail.com>
2022-08-11 17:13:40 +0300

    post forms added

--- frontend/pages/post.js
+++ frontend/pages/post.js
@@ -14,13 +14,14 @@ const post = () => {
     ],
   });
 
-  const [num, setNum] = useState(1);
-  const updateNumOfParagraphs = (e) => {
-    setNum(e.target.value);
+  // number of paragraphs
+  const [num, setNum] = useState([1]);
+  const handleClick = () => {
+    setNum((prev) => [...prev, 2]);
   };
+
   //  onChangehandler
   const onChangehandler = (e) => {
-    console.log(e.target);
     setPost((prev) => ({
       ...prev,
       [name]: e.target.value,
@@ -30,23 +31,30 @@ const post = () => {
   // submitHandler
   const submitHandler = (e) => {
     e.preventDefault();
+    var arr [];
+    num.map((item, index)=>
+      arr.push({
+        title : e.target.body{index + 1}url.mainUrl
+      })
+    )
+    setPost({
+      mainTitle: e.target.mainTitle.value,
+      category: e.target.mainTitle.category,
+      mainUrl: e.target.mainTitle.mainUrl,
+      
+      
+    });
+    
   };
 
   return (
     <div className=" flex flex-col items-center justify-center">
       <h1 className="text-5xl m-10">MAKE A POST</h1>
 
-       {/* number of paragraphs */}
-       <input
-            type="text"
-            name="num"
-            placeholder="Enter Number of paragraphs"
-            onChange={updateNumOfParagraphs}
-          />
-          
-          {/* form starts here */}
+      {/* form starts here */}
       <form onSubmit={submitHandler} className="mt-10">
-        <div className="flex flex-col space-y-3 w-80">
+        <h1 className="text-4xl text-white">Header</h1>
+        <div className="flex flex-col space-y-3 w-80 ">
           {/*  mainTitle*/}
           <input
             type="text"
@@ -69,12 +77,61 @@ const post = () => {
             placeholder="mainUrl"
             onChange={onChangehandler}
           />
+          <div className="flex flex-row space-x-8">
+            <h1 className="text-4xl text-white">BODY</h1>
+
+            <button
+              className="bg-slate-500 text-white rounded p-3"
+              onClick={handleClick}
+            >
+              ADD PARAGRAPH
+            </button>
+          </div>
 
           {/* BODY */}
-          
-         
+          {num.map((item, index) => (
+            <div className="flex flex-col space-y-3 w-80 mb-20" key={index}>
+              <input
+                type="text"
+                name={`body${index + 1}title`}
+                placeholder={`Enter  The title ${index + 1}`}
+                onChange={onChangehandler}
+              />
+              <textarea
+                className="h-40"
+                type="text"
+                name={`body${index + 1}content`}
+                placeholder={`Enter  The content ${index + 1}`}
+                onChange={onChangehandler}
+              />
+              <input
+                className="mb-10"
+                type="text"
+                name={`body${index + 1}url`}
+                placeholder={`Enter  The Imge URL ${index + 1}`}
+                onChange={onChangehandler}
+              />
+            </div>
+          ))}
         </div>
+        {/* <button
+          className="bg-slate-500 text-white rounded p-3 mt-20 mb-20"
+          onClick={submitHandler}
+        >
+          SUBMIT
+        </button> */}
+        <input
+          type="submit"
+          value="Submit"
+          className="bg-slate-500 text-white rounded p-3 mt-20 mb-20"
+        />
       </form>
+
+      {/* TEST */}
+      <div>
+        <h1>{post.mainTitle}</h1>
+        <h1>{post.category}</h1>
+      </div>
     </div>
   );
 };
