46135ad5eda5b1573ff0124c6ab30d9f40cc1ac7
Hassan-jr <abdiladifhassan115@gmail.com>
2022-08-14 12:40:22 +0300

    formik added

--- frontend/pages/post.js
+++ frontend/pages/post.js
@@ -1,153 +1,146 @@
-import React, { useState } from "react";
+import React from "react";
+import { Formik, Field, Form, ErrorMessage, FieldArray } from "formik";
+
+const initialValues = {
+  mainTitle: "",
+  mainUrl: "",
+  category: "",
+  metaData: "",
+  friends: [
+    {
+      name: "",
+      email: "",
+    },
+  ],
+};
 
 const post = () => {
-  const [Post, setPost] = useState({
-    mainTitle: "",
-    category: "",
-    mainUrl: "",
-    body: [
-      {
-        title: "",
-        content: "",
-        url: "",
-      },
-    ],
-  });
-
-  // number of paragraphs
-  // const [num, setNum] = useState([1]);
-  const handleClick = (e) => {
-    e.preventDefault();
-    // setNum((prev) => [...prev, 2]);
-    setPost({
-      body: [...Post.body, { title: "", content: "", url: "" }],
-    });
-  };
-
-  //  onChangehandler
-  const onChangehandler = (e) => {
-    if (["title", "content", "url"].includes(e.target.name)) {
-      let body = Post.body && [...Post.body];
-      body[e.target.id][e.target.name] = e.target.value;
-      setPost({ body });
-    } else {
-      setPost({ [e.target.name]: e.target.value });
-    }
-  };
-
-  // test
-
-  const hanldeClick2 = () => {
-    console.log(Post);
-  };
-
-  // submitHandler
-  const submitHandler = (e) => {
-    e.preventDefault();
-  };
-
   return (
-    <div className=" flex flex-col items-center justify-center">
-      <h1 className="text-5xl m-10">MAKE A POST</h1>
-
-      {/* form starts here */}
-      <form
-        onSubmit={submitHandler}
-        className="mt-10"
-        onChange={onChangehandler}
+    <div>
+      <h1>Invite friends</h1>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 500));
+          alert(JSON.stringify(values, null, 2));
+          // console.log(values);
+        }}
       >
-        <h1 className="text-4xl text-white">Header</h1>
-        <div className="flex flex-col space-y-3 w-80 ">
-          {/*  mainTitle*/}
-          <input
-            type="text"
-            name="mainTitle"
-            placeholder="Main Title"
-            value={Post.mainTitle}
-            // onChange={onChangehandler}
-          />
-          {/* category */}
-          <input
-            type="text"
-            name="category"
-            placeholder="category"
-            value={Post.category}
-            // onChange={onChangehandler}
-          />
+        {({ values }) => (
+          <Form>
+            {/* mainTitle */}
 
-          {/* mainUrl */}
-          <input
-            type="text"
-            name="mainUrl"
-            placeholder="mainUrl"
-            value={Post.mainUrl}
-            // onChange={onChangehandler}
-          />
-
-          {/* BUTTO FOR ADDING THE PARAGRAPHS */}
-          <div className="flex flex-row space-x-8">
-            <h1 className="text-4xl text-white">BODY</h1>
-            <button
-              className="bg-slate-500 text-white rounded p-3"
-              onClick={handleClick}
-            >
-              ADD PARAGRAPH
-            </button>
-          </div>
-
-          {/* BODY */}
-          {Post.body?.map((item, index) => (
-            <div className="flex flex-col space-y-3 w-80 mb-20" key={index}>
-              <input
-                type="text"
-                id={index}
-                name="title"
-                placeholder={`Enter  The title ${index + 1}`}
-                value={item.title}
-                // onChange={onChangehandler}
-              />
-              <textarea
-                className="h-40"
-                type="text"
-                id={index}
-                name="content"
-                placeholder={`Enter  The content ${index + 1}`}
-                value={item.content}
-                // onChange={onChangehandler}
-              />
-              <input
-                className="mb-10"
-                type="text"
-                id={index}
-                name="url"
-                placeholder={`Enter  The Imge URL ${index + 1}`}
-                value={item.url}
-                // onChange={onChangehandler}
+            <div className="col">
+              <label htmlFor="mainTitle">MainTitle</label>
+              <Field name="mainTitle" placeholder="mainTitle" type="text" />
+              <ErrorMessage
+                name="mainTitle"
+                component="div"
+                className="field-error"
               />
             </div>
-          ))}
-        </div>
-        {/* <button
-          className="bg-slate-500 text-white rounded p-3 mt-20 mb-20"
-          onClick={submitHandler}
-        >
-          SUBMIT
-        </button> */}
-        <button
-          type="submit"
-          className="bg-slate-500 text-white rounded p-3 mt-20 mb-20"
-          onClick={hanldeClick2}
-        >
-          SUBMIT
-        </button>
-      </form>
 
-      {/* TEST */}
-      <div>
-        <h1>{Post.mainTitle}</h1>
-        <h1>{Post.category}</h1>
-        <h1>{Post && Post.body && Post.body[0].title}</h1>
-      </div>
+            {/* mainUrl */}
+
+            <div className="col">
+              <label htmlFor="mainUrl">MainUrl</label>
+              <Field name="mainUrl" placeholder="mainUrl" type="text" />
+              <ErrorMessage
+                name="mainUrl"
+                component="div"
+                className="field-error"
+              />
+            </div>
+
+            {/* category */}
+            <div className="col">
+              <label htmlFor="category">category</label>
+              <Field name="category" placeholder="category" type="text" />
+              <ErrorMessage
+                name="category"
+                component="div"
+                className="field-error"
+              />
+            </div>
+
+            {/* metaData */}
+            <div className="col">
+              <label htmlFor="metaData">MetaData</label>
+              <Field name="metaData" placeholder="metaData" type="text" />
+              <ErrorMessage
+                name="metaData"
+                component="div"
+                className="field-error"
+              />
+            </div>
+                 
+                 {/* array of paragraphs or posts */}
+            <FieldArray name="friends">
+              {({ insert, remove, push }) => (
+                <div>
+                  {values.friends.length > 0 &&
+                    values.friends.map((friend, index) => (
+                      // main on div
+                      <div className="row" key={index}>
+                        {/* each object wthin the array */}
+
+                        {/* name */}
+                        <div className="col">
+                          <label htmlFor={`friends.${index}.name`}>Name</label>
+                          <Field
+                            name={`friends.${index}.name`}
+                            placeholder="Jane Doe"
+                            type="text"
+                          />
+                          <ErrorMessage
+                            name={`friends.${index}.name`}
+                            component="div"
+                            className="field-error"
+                          />
+                        </div>
+
+                               {/* email */}
+                        <div className="col">
+                          <label htmlFor={`friends.${index}.email`}>
+                            Email
+                          </label>
+                          <Field
+                            name={`friends.${index}.email`}
+                            placeholder="jane@acme.com"
+                            type="email"
+                          />
+                          <ErrorMessage
+                            name={`friends.${index}.name`}
+                            component="div"
+                            className="field-error"
+                          />
+                        </div>
+                        {/* END */}
+                        <div className="col">
+                          <button
+                            type="button"
+                            className="secondary"
+                            onClick={() => remove(index)}
+                          >
+                            X
+                          </button>
+                        </div>
+                      </div>
+                    ))}
+                  <button
+                    type="button"
+                    className="secondary"
+                    onClick={() => push({ name: "", email: "" })}
+                  >
+                    Add Friend
+                  </button>
+                </div>
+              )}
+            </FieldArray>
+            <button type="submit">Invite</button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
