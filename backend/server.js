69243881974b2e45330fe162550b1e63bc4b8997
Hassan-jr <abdiladifhassan115@gmail.com>
2022-08-19 09:43:33 +0300

    initial commit 4

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express()
 app.use (express.json())
 app.use(cors())
 app.use (express.urlencoded({extended: true}))
-const port =   process.env.PORT || 4000
+// const port =   process.env.PORT || 4000
 
 
 
@@ -32,6 +32,6 @@ if (process.env.NODE_ENV === 'production') {
   }
 
 
-app.listen (port, ()=>{
+app.listen ( process.env.PORT || 4000, ()=>{
     console.log('listening on port');
 })
