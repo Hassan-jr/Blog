562de14089e899522855c4db4699ccbedf8304b1
Hassan-jr <abdiladifhassan115@gmail.com>
2022-09-15 17:06:02 +0300

    ga added

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -2,14 +2,40 @@ import React from "react";
 import Nav from "./Nav";
 import { wrapper } from "../components/Redux/store";
 import Footer from "./Footer";
+import Script from "next/script";
 
 const Layout = ({ children }) => {
+  // <!-- Global site tag (gtag.js) - Google Analytics -->
+  // <script async src="https://www.googletagmanager.com/gtag/js?id=UA-198293761-2"></script>
+  // <script>
+  //   window.dataLayer = window.dataLayer || [];
+  //   function gtag(){dataLayer.push(arguments);}
+  //   gtag('js', new Date());
+
+  //   gtag('config', 'UA-198293761-2');
+  // </script>
+
   return (
-    <div className="flex flex-col min-h-screen">
-      <Nav />
-      {children}
-      <Footer/>
-    </div>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=UA-198293761-2`}
+      />
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+       
+           gtag('config', 'UA-198293761-2');
+        `}
+      </Script>
+      <div className="flex flex-col min-h-screen">
+        <Nav />
+        {children}
+        <Footer />
+      </div>
+    </>
   );
 };
 
